2aede6c5a3927f8ebfbdd8c93469082ce6608f6f
abhinavmishra01 <110123777+abhinavmishra01@users.noreply.github.com>
2022-07-31 14:08:20 +0530

    Datatype

---
+++ datatypes.py
@@ -0,0 +1,22 @@
+from turtle import color
+
+
+x= 1920
+y= 9205
+x2 = -39021309
+y2 = -9203
+print(x,type(x))
+print(y,type(y))
+print(x2,type(x2))
+print(y2,type(y2))
+#list is an ordered sequence of items/
+#most used 
+#Items in a list  do not to be a same type
+#declaring is straigh Forkprocess
+#seprated by commas are enclosed with []
+a= {5,10,15,20,25,30,35,40}
+vals = (1,2,2,'python')
+COLORS= {'red', 'green','pink'}
+print (a)
+print (vals)
+print (COLORS)
---
+++ operators.py
@@ -0,0 +1,13 @@
+a = 130
+b =50
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b) # float division
+
+#operators precedence
+fruits = ['Apple','Banana', 'Orange ','Pear', 'Grape']
+print('papaya'in fruits)
+print ('apple'in fruits)
+print ('Apple' in fruits)
