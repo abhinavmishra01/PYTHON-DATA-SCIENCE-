acf80dbd292657b0d45fce3b426b045ba6d03bc2
abhinavmishra01 <110123777+abhinavmishra01@users.noreply.github.com>
2022-08-07 12:24:40 +0530

    menu box

--- project.py
+++ project.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import ttk
 window = Tk ()
 window.title ("Welcome to This World")
-window.geometry ('325x250')
+window.geometry ('450x550' )
 window. configure (background ="green")
 ttk.Button (window, text="Hello Abhinav").grid()
-window.mainloop()
+window.mainloop() 
