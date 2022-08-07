6e5486cdbc53b157c9196da36b2b82088d5816df
abhinavmishra01 <110123777+abhinavmishra01@users.noreply.github.com>
2022-08-07 14:06:56 +0530

    Create user register.py

---
+++ user register.py
@@ -0,0 +1,21 @@
+from atexit import register
+from os import name
+print("Register your details")
+
+name=input("enter your name")
+email=input("enter your email")
+password=input ("enter your password")
+confrimpassword= input("enter your confrim password")
+gender=input ("enter your gender (M/F/O):")
+
+if len (name)>4 and len (name)<25:
+    if '@'in email:
+        if password==confrimpassword:
+            print ("You are registered")
+        else:
+              print ("passwords do not match")
+    else:
+        print (" email is invalid ")
+else:
+       print ('username must be between 4 and 25 chars')
+
