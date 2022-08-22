2445fc56f8a6801780a677d946b76993e3dc2866
abhinavmishra01 <110123777+abhinavmishra01@users.noreply.github.com>
2022-08-22 22:44:18 +0530

    loops

---
+++ basics/loop2.py
@@ -0,0 +1,14 @@
+
+from turtle import*
+speed(0)
+pensize(2)
+pencolor('yellow')
+bgcolor('red')
+bgcolor('black')
+for i in range(12):
+   circle(100)
+right(360/12)
+for i in range(6):
+    circle(121)
+left(360/21)
+mainloop
